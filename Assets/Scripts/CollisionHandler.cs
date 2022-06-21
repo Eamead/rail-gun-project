54ce284292e70a8b06bace925ff2de0f0a2cc2b5
FudgeMerchant <107005471+FudgeMerchant@users.noreply.github.com>
2022-06-20 21:51:23 -0400

    Player Explosion and Gravity

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other) 
+    
+    [SerializeField] float delay = 1f;
+    [SerializeField] ParticleSystem explosion;
+    
+    Rigidbody rb;
+
+    void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+    
+    
+    void OnTriggerEnter(Collider other)
     {
-        //if (other.gameObject.tag == "Player") 
-        //{
+        if (other.gameObject.tag != "Player") 
+        {
+            explosion.Play();
+            rb.useGravity = true;
+            GetComponent<PlayerControls>().enabled = false;
+            GetComponent<SphereCollider>().enabled = false;
+            Invoke("DeathorSomethinghahahaarentyoufuckingclever", delay);           
             Debug.Log(this.name + "has collided with " + other.gameObject.name);
-        //}
+        }
     }
 
-    void OnTriggerEnter(Collider other) 
+    void DeathorSomethinghahahaarentyoufuckingclever()
     {
-        Debug.Log($"{this.name} has triggered {other.gameObject.name}");
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
+
 }
