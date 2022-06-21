d7d739358910339a5d11728b1a0c1376ebdd9954
FudgeMerchant <107005471+FudgeMerchant@users.noreply.github.com>
2022-06-21 17:04:43 -0400

    Physic Deaths for Animated Objects

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,31 +6,42 @@ public class Enemy : MonoBehaviour
 {
     Rigidbody rb;
     Vector3 forceDirection;
+    Animator anim;
 
     [SerializeField] GameObject boom;
     [SerializeField] Transform parent;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
         //var boomVisual = boom.GetComponent<ParticleSystem>();
         //boomVisual.Stop();
     }
 
+
     void OnParticleCollision(GameObject other) 
     {
-        float forceX = -10f;
-        float forceY = 0;
-        float forceZ = -10f;
-        forceDirection = new Vector3(forceX, forceY, forceZ);
+        if (anim.enabled == true)
+            {
+                anim.enabled = false;
+            }
+        else
+            {
+                float forceX = -10f;
+                float forceY = 0;
+                float forceZ = -10f;
+                forceDirection = new Vector3(forceX, forceY, forceZ);
 
         
-        GameObject explode = Instantiate(boom, transform.position, Quaternion.identity);
-        explode.transform.parent = parent;
-        //var boomVisual = boom.GetComponent<ParticleSystem>();
-        //boomVisual.Play();
+                GameObject explode = Instantiate(boom, transform.position, Quaternion.identity);
+                explode.transform.parent = parent;
+                //var boomVisual = boom.GetComponent<ParticleSystem>();
+                //boomVisual.Play();
 
-        rb.useGravity = true;
-        rb.AddRelativeForce(forceDirection, ForceMode.Impulse);
-        Destroy(gameObject, 2);
+                rb.useGravity = true;
+                rb.AddRelativeForce(forceDirection, ForceMode.Impulse);
+                Destroy(gameObject, 2);
+            }
     }
 }
